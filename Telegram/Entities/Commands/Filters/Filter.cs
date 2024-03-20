676beb01f8f6e6adceb371098aabd9cd1567b166
Nick Krylov <kkrulov2006@gmail.com>
2024-03-20 14:00:28 +0300

    Renaming classes

--- Telegram/Entities/Commands/Filters/Filter.cs
+++ Telegram/Entities/Commands/Filters/Filter.cs
@@ -1,10 +1,11 @@
 ﻿namespace Telegramchik.Commands.Filters;
 
-public interface IFilters
+public class Filter : IFilter
 {
-    public string Text { get; set; }
+    public string? Text { get; set; }
     public byte Type { get; set; }
-    public string FileId { get; set; }
+    public string? FileId { get; set; }
     public string Name { get; set; }
+
 
 }
--- Telegram/Entities/Commands/Filters/IFilter.cs
+++ Telegram/Entities/Commands/Filters/IFilter.cs
@@ -1,11 +1,10 @@
 ﻿namespace Telegramchik.Commands.Filters;
 
-public class Filters : IFilters
+public interface IFilter
 {
-    public string? Text { get; set; }
+    public string Text { get; set; }
     public byte Type { get; set; }
-    public string? FileId { get; set; }
+    public string FileId { get; set; }
     public string Name { get; set; }
-
 
 }
