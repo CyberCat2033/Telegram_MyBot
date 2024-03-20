bc4e8d922a512c4d277bb920850bd28e654e8183
Nick Krylov <kkrulov2006@gmail.com>
2024-03-21 00:59:04 +0300

    Nothing

--- Telegram/Entities/Commands/Filters/Filters.cs
+++ Telegram/Entities/Commands/Filters/Filters.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Telegram.Bot.Types;
 
 namespace Telegramchik.Commands.Filters;
 
@@ -10,8 +11,14 @@ public class Filters
 {
     public Dictionary<string, Filter> Filters_Dict = new();
 
+    public async Task Add(Message message) => await Add(new Filter(message));
+
     public async Task Add(Filter filter)
     {
-
+        await Task.Run(() =>
+        {
+            if (Filters_Dict.TryAdd(filter.Name, filter)) return;
+            Filters_Dict[filter.Name] = filter;
+        });
     }
 }
