22f676e791093f474ffe4718be2ff40c91b31ce5
Nick Krylov <kkrulov2006@gmail.com>
2024-03-23 22:35:37 +0300

    Nothing

---
+++ Telegram/Entities/Commands/Filters/Stop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegramchik;
+
+namespace Telegramchik.Commands.Filters;
+
+public class Stop : TelegramCommands
+{
+    public Stop(string Command, string Description = "") : base(Command, Description)
+    {
+    }
+
+    public override Task ExecuteAsync(Message message, ITelegramBotClient botClient, CancellationToken CT)
+    {
+        throw new NotImplementedException();
+    }
+}
