e63843ffad638e32fd6aba017bfd99a4dc3599f1
Nick Krylov <kkrulov2006@gmail.com>
2024-04-03 16:35:58 +0300

    Added Settings

---
+++ Telegram/Entities/Settings/Settings.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+using Telegramchik.Commands.Filters;
+
+namespace Telegramchik.Settings;
+
+public class Settings
+{
+    private ConcurrentDictionary<string, Filter> Filters_Dict = new();
+
+    #region Filters
+
+    public async Task AddFilter(Message message)
+        => await AddFilter(new Filter(message));
+
+    public async Task AddFilter(Filter filter)
+    {
+        await Task.Run(() =>
+        {
+            Filters_Dict[filter.Name] = filter;
+        });
+    }
+
+    public async Task RemoveFilter(Message message)
+    {
+        await Task.Run(() =>
+        {
+            if (!Filters_Dict.TryRemove(message.Text.Split()[1], out var val))
+            {
+                throw new ArgumentException("Filters doesn`t contains such a keyword");
+            }
+        });
+    }
+
+    public bool TryGetFilter(string key, out Filter fl)
+    {
+        return Filters_Dict.TryGetValue(key, out fl);
+
+    }
+    #endregion
+
+    #region Greating
+
+    #endregion
+}
---
+++ Telegram/Entities/Settings/SettingsFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telegramchik.Settings;
+
+public static  class SettingsFactory
+{
+    private static ConcurrentDictionary<long, Settings> _settings = new();
+
+    public static void Add(long ChatId, Settings settings)
+    {
+        _settings[ChatId] = settings;
+    }
+
+    public static Settings Get(long ChatId)
+    {
+        if(_settings.TryGetValue(ChatId, out var settings))
+        {
+            return settings;
+        }
+        throw new ArgumentException("There`s no settings with such ID");
+    }
+}
