10e606e42ad6fa25a5d7a0dd92255b18a8ab437f
CyberCat <kkrulov2006@gmail.com>
2024-04-16 15:46:28 +0300

    nothing

--- Telegram/Entities/Commands/Press F/FCommand.cs
+++ Telegram/Entities/Commands/Press F/FCommand.cs
@@ -5,39 +5,43 @@ namespace Telegramchik.Commands;
 
 public class FCommand : TelegramBotCommands
 {
-    private readonly string[] Stcikers_IDs = {
-    "CAACAgIAAxkBAAICMmX62PS9RFDMHb2On7G9DOjKRbnWAAIMAQACTptkAmOSrBs0ItNHNAQ",
-    "CAACAgIAAxkBAAICNWX62QEWGPMfuMwT5vrKEEHnAfXpAAIdAQACTptkAnofe0zzYUy2NAQ",
-    "CAACAgIAAxkBAAICOGX62Q4Pqga0wRQJQ8318dMix5z0AAJJAQACTptkAmgp7D2NPAz-NAQ",
-    "CAACAgIAAxkBAAICO2X62SU7RssdPTUVSMllmI8_jv5UAAK7AANOm2QCTTNNKc65rHw0BA",
-    "CAACAgIAAxkBAAICPmX62TyzZAST_iSxB_40KgXAynKzAALHFQACOk4JSQFAjkLpBTNnNAQ",
-    };
+	private int indexer;
+	private readonly string[] Stcikers_IDs = {
+	"CAACAgIAAxkBAAICMmX62PS9RFDMHb2On7G9DOjKRbnWAAIMAQACTptkAmOSrBs0ItNHNAQ",
+	"CAACAgIAAxkBAAICNWX62QEWGPMfuMwT5vrKEEHnAfXpAAIdAQACTptkAnofe0zzYUy2NAQ",
+	"CAACAgIAAxkBAAICOGX62Q4Pqga0wRQJQ8318dMix5z0AAJJAQACTptkAmgp7D2NPAz-NAQ",
+	"CAACAgIAAxkBAAICO2X62SU7RssdPTUVSMllmI8_jv5UAAK7AANOm2QCTTNNKc65rHw0BA",
+	"CAACAgIAAxkBAAICPmX62TyzZAST_iSxB_40KgXAynKzAALHFQACOk4JSQFAjkLpBTNnNAQ",
+	};
 
-    public FCommand(string Command, string Description) : base(Command, Description) { }
+	public FCommand(string Command, string Description) : base(Command, Description) { }
 
-    private InputFileId GetRandomSticker()
-    {
+	private InputFileId RandomSticker => InputFile.FromFileId(Stcikers_IDs[Get()]);
 
-        return InputFile.FromFileId(Stcikers_IDs[new Random().Next(0, Stcikers_IDs.Length)]);
-    }
+	private int Get()
+	{
+		var tm_indexer = new Random().Next(0, Stcikers_IDs.Length);
+		indexer = indexer == tm_indexer ? (tm_indexer + 1) % Stcikers_IDs.Length - 1 : tm_indexer;
+		return indexer;
+	}
 
-    public override async Task ExecuteAsync(Message message, ITelegramBotClient botClient, CancellationToken CancellationToken)
-    {
-        long ChatId = message.Chat.Id;
+	public override async Task ExecuteAsync(Message message, ITelegramBotClient botClient, CancellationToken CancellationToken)
+	{
+		long ChatId = message.Chat.Id;
 
-        await botClient.SendStickerAsync(
-            chatId: ChatId,
-            sticker: GetRandomSticker(),
-            cancellationToken: CancellationToken
-            );
-        await Task.Delay(15);
+		await botClient.SendStickerAsync(
+			chatId: ChatId,
+			sticker: RandomSticker,
+			cancellationToken: CancellationToken
+			);
+		await Task.Delay(100);
 
-        await botClient.DeleteMessageAsync(
-            chatId: ChatId,
-            messageId: message.MessageId,
-            cancellationToken: CancellationToken
-            );
+		await botClient.DeleteMessageAsync(
+			chatId: ChatId,
+			messageId: message.MessageId,
+			cancellationToken: CancellationToken
+			);
 
 
-    }
+	}
 }
